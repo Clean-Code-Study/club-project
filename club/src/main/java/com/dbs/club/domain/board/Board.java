1cf9e1cb37155d8d8c1dea02bf994e91c18c2d0c
seula123 <sla6239@gmail.com>
2024-07-30 18:12:59 +0900

    board 엔티티 생성한다

---
+++ club/src/main/java/com/dbs/club/domain/board/Board.java
@@ -0,0 +1,54 @@
+package com.dbs.club.domain.board;
+
+import com.dbs.club.domain.common.BaseEntity;
+import com.dbs.club.domain.common.RegisterDeleteState;
+import com.dbs.club.domain.member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Board extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn( name = "member_id", referencedColumnName = "id")
+    private Member member;
+
+    @Column(name = "title", nullable = false, length = 30)
+    private String title;
+
+    @Column(name = "content", nullable = false, length = 1500)
+    private String content;
+
+    @Column(name = "status", nullable = false, length = 30)
+    private RegisterDeleteState status;
+
+
+    //생성자
+    public Board(Member member, String title, String content, RegisterDeleteState status) {
+
+        this.member = member;
+
+        this.title = title;
+
+        this.content = content;
+
+        this.status = status;
+    }
+
+    //초기화 메소드
+    public static Board init(Member member, String title, String content, RegisterDeleteState status) {
+
+        return new Board(member,title,content,status);
+    }
+
+
+
+}
